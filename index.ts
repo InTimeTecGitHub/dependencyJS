0ca6d2677d3979a03a3572dc14c9808d81f22343
ManishKumawat <manish.kumawat@intimetec.com>
2019-01-28 17:58:23 +0530

    some fixes

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
 
-import {appConfig, Env} from "./src/config/Configuration";
 import {ComponentRegistry} from "./src/ComponentRegistry";
 export {ComponentRegistry};
-declare var window: any;
-if (appConfig.Env === Env.BROWSER) {
-    window.ComponentRegistry = ComponentRegistry;
-}
